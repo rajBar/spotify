0a7ba0d8ec91af9978f387ed41771218639d29ec
Gulraj Bariah <raj.uni@outlook.com>
2023-10-05 12:27:18 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const fetch = require("node-fetch");
+
+const getArtistsDetails = async artist => {
+    const data = await fetch('https://api.spotify.com/v1/search?q=' + artist + '&type=artist',{
+        method: 'GET',
+        contentType: '',
+        headers: {
+		    Authorization: 'Bearer BQBi8yxCV7_XoCiQR04n4ldiIums4J8sjKLqsekWYRwsq5kPs9cHdhDXWNKUN7EiKLiM94KhEZgYVDQvvIJdg0nz0mWhKy1pPynTcGiRxiKwphYKJiw'      
+    }}).then(res => res.json())
+
+    const followers = data.artists.items[0].followers.total;
+    const genres = data.artists.items[0].genres;
+    const popularity = data.artists.items[0].popularity;
+    const uri = data.artists.items[0].uri;
+    const id = data.artists.items[0].id;
+
+    return {
+        artist, 
+        id,
+        followers,
+        genres,
+        popularity,
+        uri
+    }
+}
+
+const generateData = async () => {
+    const artists = ['Taylor Swift','Drake','Ed Sheeran','The Weeknd','Bad Bunny','Eminem','Justin Bieber','Ariana Grande','BTS','Kanye West','Rihanna','Post Malone','Billie Eilish','Dua Lipa','Adele','Coldplay','Beyonce','Bruno Mars','Michael Jackson','Imagine Dragons','Maroon 5','Lana Del Rey','Elton John','Shawn Mendes']
+
+    const allData = [];
+    
+    for(artist in artists){
+        const name = artists[artist]
+        const data = await getArtistsDetails(name);
+        allData.push(data);
+    };
+
+    return allData
+}
+
+const main = async () => {    
+    const spotifyData = await generateData();
+
+    spotifyData.forEach(d => console.log(d));
+}
+
+main()
